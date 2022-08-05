09dd540402f8e9d2959fb997b587571ae51858ff
Tatsuya Deguchi <t.deg84@gmail.com>
2022-08-05 14:38:39 +0900

    スマートコントラクトアドレスを入れる

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import "babel-polyfill";
 import Web3 from "web3";
 
 // ブロックチェーンにデプロイしたスマートコントラクトのアドレス
-var smartContractAddress = "0xf55c6c6C7..."; // ← Hello コントラクトのコントラクトアドレスを指定
+var smartContractAddress = "0x711DD88695EdD624751dbF34fdf6BB47685a87d3";
 
 // ABI(Application Binary Interface) はブロックチェーンの外からコントラクトを利用するための
 // インターフェースの定義です。
