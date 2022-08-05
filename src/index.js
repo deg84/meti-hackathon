c9735b0cfe867ff0f4696e15da44e5106e48114a
Tatsuya Deguchi <t.deg84@gmail.com>
2022-08-05 14:38:51 +0900

    abiを設定

--- src/index.js
+++ src/index.js
@@ -6,4 +6,43 @@ var smartContractAddress = "0x711DD88695EdD624751dbF34fdf6BB47685a87d3";
 
 // ABI(Application Binary Interface) はブロックチェーンの外からコントラクトを利用するための
 // インターフェースの定義です。
-var abi = []; // ← build/contracts/Hello.json の abi 要素で置き換える
+var abi = [
+  {
+    inputs: [
+      {
+        internalType: "string",
+        name: "initMessage",
+        type: "string",
+      },
+    ],
+    stateMutability: "nonpayable",
+    type: "constructor",
+  },
+  {
+    inputs: [],
+    name: "message",
+    outputs: [
+      {
+        internalType: "string",
+        name: "",
+        type: "string",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+    constant: true,
+  },
+  {
+    inputs: [
+      {
+        internalType: "string",
+        name: "newMessage",
+        type: "string",
+      },
+    ],
+    name: "update",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+];
