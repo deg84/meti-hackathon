00243d202f96680fb961a252781487a2a6793c85
Tatsuya Deguchi <t.deg84@gmail.com>
2022-08-05 14:40:52 +0900

    スマートコントラクトのアドレス、ABIを再度設定する

--- src/index.js
+++ src/index.js
@@ -2,11 +2,50 @@ import "babel-polyfill";
 import Web3 from "web3";
 
 // ブロックチェーンにデプロイしたスマートコントラクトのアドレス
-var smartContractAddress = "";
+var smartContractAddress = "0x711DD88695EdD624751dbF34fdf6BB47685a87d3";
 
 // ABI(Application Binary Interface) はブロックチェーンの外からコントラクトを利用するための
 // インターフェースの定義です。
-var abi = [];
+var abi = [
+  {
+    inputs: [
+      {
+        internalType: "string",
+        name: "initMessage",
+        type: "string",
+      },
+    ],
+    stateMutability: "nonpayable",
+    type: "constructor",
+  },
+  {
+    inputs: [],
+    name: "message",
+    outputs: [
+      {
+        internalType: "string",
+        name: "",
+        type: "string",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+    constant: true,
+  },
+  {
+    inputs: [
+      {
+        internalType: "string",
+        name: "newMessage",
+        type: "string",
+      },
+    ],
+    name: "update",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+];
 
 let myAccount;
 let web3;
